39dd6b273c99495c1b9df20a7699a40d0ef06c06
ramalakeven <143877366+ramalakeven@users.noreply.github.com>
2023-09-15 23:01:05 +0300

    mass.py

--- mass.py
+++ mass.py
@@ -7,5 +7,5 @@ r = float(input("Введите расстояние до объекта в ки
 G = 6.676  *  math.pow(10,-11)
 m2 = 5.976   *  math.pow(10,24)
 F = (G * m1 * m2 )/ (math.pow(r,2) * math.pow(10,20))
-print(F)
+print(F, "* 10^20 Н")
 
