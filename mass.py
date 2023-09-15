660765a1ff7afafc0f09b4dff8beaff87d5d8a3d
ramalakeven <143877366+ramalakeven@users.noreply.github.com>
2023-09-15 22:39:28 +0300

    Add files via upload

---
+++ mass.py
@@ -0,0 +1,11 @@
+import math, datetime
+
+m1 = float(input("Введите массу объекта деленную на 10^22:   ")) *math.pow(10,22)
+
+r = float(input("Введите расстояние до объекта в километрах:   ")) *1000
+
+G = 6.676  *  math.pow(10,-11)
+m2 = 5.976   *  math.pow(10,24)
+F = (G * m1 * m2 )/ (math.pow(r,2) * math.pow(10,20))
+print(F)
+
